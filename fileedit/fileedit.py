b585ecc46e75c3f0c3d19761fe185ef621378470
kr8gz <47776344+kr8gz@users.noreply.github.com>
2020-10-05 14:24:11 +0200

    Update fileedit.py
    ByteFile support

--- fileedit/fileedit.py
+++ fileedit/fileedit.py
@@ -11,9 +11,11 @@ Thank you for using FileEdit! Type fileedit_help() for help.
 def fileedit_help():
     print("""
 Here are all the things you can do with FileEdit:
-
 f = File("test.txt")
 * Creates a new File object from the file "test.txt".
+--------------------------------------------------------------------------------
+bf = ByteFile("test.docx")
+* Creates a new ByteFile object from the file "test.docx".
 --------------------------------------------------------------------------------
 cont = f.contents
 * Returns a list where one element is one line of the file.
@@ -257,5 +259,183 @@ class File:
     def clear(self):
         
         with open(self.name, "w"):
+            pass
+        self.update_contents()
+
+
+class ByteFile:
+
+    def __init__(self, path):
+
+        if not os.path.isfile(path):
+            try:
+                with open(path, "wb+"):
+                    pass
+            except OSError:
+                raise InvalidFileName(path)
+        
+        self.name = path
+        self.contents = ""
+
+        self.update_contents()
+
+    def __del__(self):
+
+        os.remove(self.name)
+      
+    def update_contents(self):
+
+        with open(self.name, "rb") as f:
+            self.contents = f.readlines()
+            self.contents.insert(0, b"")
+            self.contents = [line.strip(b"\n") for line in self.contents]
+
+    def write(self, text, *args, **kwargs):
+
+        self.update_contents()
+
+        line = None
+
+        if args or kwargs:
+            if args:
+                l = args[0]
+            elif "line" in kwargs:
+                l = kwargs['line']
+            if type(l) not in [str, int]:
+                raise InvalidArgument(f"{l} is not a valid line number")
+
+            try:
+                l = int(l)
+                if l < 1:
+                    raise InvalidArgument(f"{l} is not a valid line number")
+            except ValueError:
+                raise InvalidArgument(f"{l} is not a valid line number")
+            line = l
+        
+        if type(text) in [tuple, list]:
+            
+            with open(self.name, "wb") as f:
+                if line:
+                    self.contents = self.contents[:line] + text + self.contents[line:]
+                f.writelines([line + b"\n" for line in self.contents[1:]])
+                if not line:
+                    f.writelines([line.strip(b"\n") + b"\n" for line in text])
+                
+            self.update_contents()
+
+        else:
+
+            with open(self.name, "wb") as f:
+                if line:
+                    self.contents.insert(line, str(text))
+                f.writelines([line + b"\n" for line in self.contents[1:]])
+                if not line:
+                    f.write(str(text))
+                
+            self.update_contents()
+
+    def overwrite(self, text, *args, **kwargs):
+
+        self.update_contents()
+
+        line = None
+
+        if args or kwargs:
+            if args:
+                l = args[0]
+            elif "line" in kwargs:
+                l = kwargs['line']
+            if type(l) not in [str, int]:
+                raise InvalidArgument(f"{l} is not a valid line number")
+
+            try:
+                l = int(l)
+                if l < 1:
+                    raise InvalidArgument(f"{l} is not a valid line number")
+            except ValueError:
+                raise InvalidArgument(f"{l} is not a valid line number")
+            line = l
+        
+        if type(text) in [tuple, list]:
+            
+            with open(self.name, "wb") as f:
+                if line:
+                    raise InvalidArgument(f"Cannot insert {type(text)} into line")
+                else:
+                    f.writelines([line.strip(b"\n") + b"\n" for line in text])
+                
+            self.update_contents()
+
+        else:
+
+            with open(self.name, "wb") as f:
+                if line:
+                    self.contents[line] = str(text)
+                    f.writelines([line + b"\n" for line in self.contents[1:]])
+                else:
+                    f.write(str(text))
+                
+            self.update_contents()
+
+    def find(self, to_find):
+
+        self.update_contents()
+
+        if type(to_find) not in [str, int, float]:
+            raise InvalidArgument(f"{type(to_find)} cannot be searched for")
+        if "\n" in str(to_find):
+            raise InvalidArgument("The string to find must not contain a newline")
+
+        for line in self.contents:
+            if str(to_find) in line:
+                return self.contents.index(line)
+                break
+        else:
+            return None
+
+    def replace(self, old, new, *args, **kwargs):
+
+        self.update_contents()
+        
+        line = None
+
+        if args or kwargs:
+            if args:
+                l = args[0]
+            elif "line" in kwargs:
+                l = kwargs['line']
+            if type(l) not in [str, int]:
+                raise InvalidArgument(f"{l} is not a valid line number")
+
+            try:
+                l = int(l)
+                if l < 1:
+                    raise InvalidArgument(f"{l} is not a valid line number")
+            except ValueError:
+                raise InvalidArgument(f"{l} is not a valid line number")
+            line = l
+
+        if type(old) not in [str, int, float]:
+            raise InvalidArgument(f"{type(old)} cannot be searched for")
+        if type(new) not in [str, int, float]:
+            raise InvalidArgument(f"Cannot replace text with {type(new)}")
+        if "\n" in str(old):
+            raise InvalidArgument("The string to find must not contain a newline")
+        if "\n" in str(new):
+            raise InvalidArgument("The string to replace with must not contain a newline")
+
+        if line:
+            with open(self.name, "wb") as f:
+                self.contents[line] = self.contents[line].replace(str(old), str(new))
+                f.writelines([line.strip(b"\n") + b"\n" for line in text])
+        else:
+            with open(self.name, "wb") as f:
+                f.writelines([line.replace(str(old), str(new)) + b"\n" for line in self.contents])
+
+        self.update_contents()
+
+    def clear(self):
+        
+        with open(self.name, "wb"):
             pass
         self.update_contents()
